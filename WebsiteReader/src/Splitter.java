98ebd75dd0061774ae796e232e6aaa8632b04288
ros_npswedberg <ros_npswedberg@WROSRM31919.seattleschools.org>
2013-10-23 09:26:36 -0700

    changing spliiter

--- WebsiteReader/src/Splitter.java
+++ WebsiteReader/src/Splitter.java
@@ -4,16 +4,31 @@ import java.util.List;
 
 public class Splitter {
 	
-	public static List<String> split (String x){
-	String[] xs = x.split("<");
-	ArrayList<String> sorted = new ArrayList();	
-	for (int i = 0; i < xs.length; i++){
-			if (!xs[i].isEmpty() && xs[i].charAt(xs[i].length() -1) != '>' ){
-				sorted.add(xs[i]);
+/*	public static List<String> split (String x){
+		String[] xs = x.split("<");
+		ArrayList<String> sorted = new ArrayList();	
+		for (int i = 0; i < xs.length; i++){
+			if(!xs[i].isEmpty()){
+				boolean isTrash = false;
+				for (int j = 0; j < xs[i].length(); j++){
+					if (xs[i].charAt(j) == '>'){
+						xs[i]
+						
+					}
+				}
 			}
-			
 		}
-	return sorted;
+		return sorted;
+	}*/
+	public static List<String> split (String x){
+		String sorted;
+		for (int i = 0; i < x.length(); i++){
+			if (x.charAt(i) == '<'){
+				String y = x.substring(x.charAt(i), x.indexOf('>'));
+				sorted.x.replace(y, "");
+			}
+		}
 	}
+	
 
 }
